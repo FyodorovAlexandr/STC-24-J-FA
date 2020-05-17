8f294ca4687a1eec5eb73e899644979f8b741829
Fyodorov Alexandr <11akbars@inbox.ru>
2020-05-17 14:06:52 +0300

    Lesson21

--- src/com/fyodorov/lesson21/Task2.java
+++ src/com/fyodorov/lesson21/Task2.java
@@ -6,21 +6,25 @@ public class Task2 {
         String str = "Hello world!";
         System.out.println(str);
         char[] reverseArray1 = str.toCharArray();
-        System.out.print("1. Массив в обратном порядке: ");
-        for (int i = reverseArray1.length - 1; i >= 0; i--)
-            System.out.print(reverseArray1[i]);
-        System.out.println();
+
         System.out.println();
 
-            int[] reverseArray2 = {1, 2, 3, 4, 5};
-            for (int i = 0; i < reverseArray2.length; i++)
-                System.out.print(reverseArray2[i] + " ");
+        for (int i = 0; i < reverseArray1.length / 2; i++) {
+            char tmp = reverseArray1[i];
+            reverseArray1[i] = reverseArray1[reverseArray1.length - 1 - i];
+            reverseArray1[reverseArray1.length - 1 - i] = tmp;
 
-            System.out.print("\n2. Массив в обратном порядке: ");
-            for (int i = reverseArray2.length - 1; i >= 0; i--) {
-                System.out.print(reverseArray2[i] + " ");
-            }
+            System.out.print(i + " Текущее состояние массива: ");
+            for (int j = 0; j < reverseArray1.length; j++)
+                System.out.print(reverseArray1[j]);
+            System.out.println();
+
         }
+
+        System.out.print("Массив в обратном порядке: ");
+        for (int j = 0; j < reverseArray1.length; j++)
+            System.out.print(reverseArray1[j]);
+        System.out.println();
+
     }
-
-
+}
