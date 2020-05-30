b2284079aa5586e816494ebbc45d23fb053aafc5
Fyodorov Alexandr <11akbars@inbox.ru>
2020-05-30 18:53:24 +0300

    Lesson27

--- src/com/fyodorov/lesson27/Task2.java
+++ src/com/fyodorov/lesson27/Task2.java
@@ -6,19 +6,19 @@ package com.fyodorov.lesson27;
  */
 
 public class Task2 {
-        private static int f ( int index){
+        private static int fibonacci(int index){
             if (index < 1) {
                 return 0;
             } else if (index == 1) {
                 return 1;
             } else {
-                return f(index - 1) + f(index - 2);
+                return fibonacci(index - 1) + fibonacci(index - 2);
             }
         }
 
         public static void main (String[]args){
             for (int i = 0; i <= 10; i++) {
-                System.out.print(f(i) + " ");
+                System.out.print(fibonacci(i) + " ");
             }
         }
     }
