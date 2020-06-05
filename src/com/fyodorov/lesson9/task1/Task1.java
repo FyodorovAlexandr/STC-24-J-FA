4f702ad40c8cd9bbeeb5d1042b75f25ecf374c53
Fyodorov Alexandr <11akbars@inbox.ru>
2020-06-05 13:40:21 +0300

    Lesson9

--- src/com/fyodorov/lesson9/task1/Task1.java
+++ src/com/fyodorov/lesson9/task1/Task1.java
@@ -1,13 +1,19 @@
 package com.fyodorov.lesson9.task1;
-
+/**
+ * Класс {@code Task1} Написать абстрактный класс Animal с абстрактным методом getName.
+ * Сделать несколько классов животных, наследников Animal. Метод getName должен выводит название каждого животного.
+ * @author Alexandr Fyodorov
+ */
 abstract class Animal {
     private String name;
     public String getName() {
         return name;
     }
+
     public Animal(String name){
         this.name = name;
     }
+
     public abstract void display();
 }
 
@@ -31,8 +37,8 @@ class Cat extends Animal {
 
 public class Task1 {
     public static void main(String[] args) {
-        Dog dog = new Dog("Dog");
-        Cat cat = new Cat("Cat");
+        Animal dog = new Dog("Dog");
+        Animal cat = new Cat("Cat");
         dog.display();
         cat.display();
     }
