6e65949a73233e39574fa649b46c54f4b28890c5
Fyodorov Alexandr <11akbars@inbox.ru>
2020-06-08 16:29:40 +0300

    Lesson30

--- src/com/fyodorov/lesson30/Task1.java
+++ src/com/fyodorov/lesson30/Task1.java
@@ -2,32 +2,33 @@ package com.fyodorov.lesson30;
 
 import java.util.*;
 /**
+ * Класс {@code Task1}
  * Напишите программу для поиска первого неповторяющегося символа в строке.
  * Например, первый неповторяющийся символ в «total» равен «o», а первый неповторяющийся символ в «teter» равен «r».
+ * @author Alexandr Fyodorov
  */
 public class Task1 {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите строку для поиска первого неповторяющегося символа" );
+        System.out.println("Введите строку для поиска первого неповторяющегося символа: ");
         String str = scanner.nextLine();
 
         Character character = findFirstNonRepeatingChar(str);
-        System.out.println("Первый неповторяющийся элемент: " + character);
+        System.out.println("Первый неповторяющийся элемент: "+character);
     }
 
 // Метод ищет первый неповторяющийся элемент в строке
-    public static Character findFirstNonRepeatingChar(String s){
+    public static Character findFirstNonRepeatingChar(String s) {
 
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer>map = new HashMap<>();
         int i;
         Character character;
         int length = s.length();
 
 // Добавление элменетов в HashMap
-        for (i = 0; i < length; i++){
+        for(i = 0; i < length; i++){
             character = s.charAt(i);
-            if (map.containsKey(character)){
+            if(map.containsKey(character)){
                 map.put(character, map.get(character)+1);
             }else
                 map.put(character, 1);
@@ -43,5 +44,3 @@ public class Task1 {
         return null;
     }
 }
-
-
