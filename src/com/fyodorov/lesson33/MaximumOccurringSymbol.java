afafaa57fb709b4bdc2aabd35f6999746503ced1
Fyodorov Alexandr <11akbars@inbox.ru>
2020-06-09 15:00:38 +0300

    Lesson33

---
+++ src/com/fyodorov/lesson33/MaximumOccurringSymbol.java
@@ -0,0 +1,38 @@
+package com.fyodorov.lesson33;
+
+import java.util.*;
+/**
+ * Класс {@code MaximumOccurringSymbol} Вывести максимально встречающийся символ в строке.
+ * Пример:
+ * This is test message
+ * Character: s has occurred maximum times in String: 5
+ * @author Alexandr Fyodorov
+ */
+public class MaximumOccurringSymbol {
+    public static void main(String[] args) {
+        System.out.println("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        Map<Character, Integer> occurrences = new HashMap<>();
+
+        for(char c: string.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if (!occurrences.containsKey(c))
+                    occurrences.put(c, 1);
+                else
+                    occurrences.put(c, occurrences.get(c) + 1);
+            }
+        }
+
+            int maxValueInMap = (Collections.max(occurrences.values()));
+            for(Map.Entry<Character, Integer> entry: occurrences.entrySet()){
+                int count = 0;
+                count += entry.getValue();
+                if(entry.getValue() == maxValueInMap){
+                    System.out.println("Character: "+entry.getKey()+" has occurred maximum times in String: "+count);
+                }
+            }
+        }
+    }
+
