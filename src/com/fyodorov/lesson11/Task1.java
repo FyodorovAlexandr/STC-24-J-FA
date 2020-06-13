ecae93228c8e38b1702f257edc793c6fb26c0d9e
Fyodorov Alexandr <11akbars@inbox.ru>
2020-06-13 15:01:22 +0300

    Lesson11

--- src/com/fyodorov/lesson11/Task1.java
+++ src/com/fyodorov/lesson11/Task1.java
@@ -1,9 +1,25 @@
 package com.fyodorov.lesson11;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Класс {@code Task1} Напишите программу которая получает на вход некую строку, после она вызывает метод,
+ * заменяющий в строке все вхождения слова «бяка» на «вырезано цензурой» и выводит результат в консоль!
+ * @author Alexandr Fyodorov
+ */
 public class Task1 {
     public static void main(String[] args) {
-        String source = "В строке есть слово бяка. Слово бяка запрещено.";
-        source = source.replaceAll("бяка", "-вырезано цензурой-");
-        System.out.println(source);
+        censorPattern();
+    }
+
+    public static void censorPattern(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку для проверки: ");
+        String text = in.nextLine();
+        Pattern censor = Pattern.compile("бяка(\\w*)", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+        Matcher replaceCensor = censor.matcher(text);
+        String newText = replaceCensor.replaceAll("вырезано цензурой ");
+        System.out.println(newText);
     }
 }
