295a3ec60acb28685869d4e3c64ed1322f5a6a3b
Fyodorov Alexandr <11akbars@inbox.ru>
2020-06-23 12:52:47 +0300

    Lesson24

--- src/com/fyodorov/lesson24/RemoveEvenLength.java
+++ src/com/fyodorov/lesson24/RemoveEvenLength.java
@@ -1,7 +1,13 @@
 package com.fyodorov.lesson24;
 
 import java.util.*;
-
+/**
+ * Класс {@code RemoveEvenLength} Написать метод, который возвращает множество,
+ * в котором удалены все элементы четной длины из исходного множества.
+ * public Set<String> removeEvenLength(Set<String> set);
+ * Например, для множества ["foo", "buzz", "bar", "fork", "bort", "spoon", "!", "dude"] метод вернет ["foo", "bar", "spoon", "!"]
+ * @author Alexandr Fyodorov
+ */
     public class RemoveEvenLength {
         public static void main(String[] args) {
 
